51a799b723a25d210c74c9beef0b9d777650402e
Jack Roennfeldt <s200099@students.aie.edu.au>
2021-02-05 09:38:44 +1030

    Fleshed out object and scene functions

--- CPPPhysicsSimulation/src/PhysicsObject.h
+++ CPPPhysicsSimulation/src/PhysicsObject.h
@@ -1,5 +1,12 @@
 #pragma once
+#include <glm.hpp>
+
 class PhysicsObject
 {
-
+protected:
+	PhysicsObject(){}
+public:
+	virtual void FixedUpdate(glm::vec2 gravity, float timeStep) = 0;
+	virtual void Draw() = 0;
+	virtual void ResetPosition() {};
 };
--- CPPPhysicsSimulation/src/PhysicsScene.cpp
+++ CPPPhysicsSimulation/src/PhysicsScene.cpp
@@ -1,10 +1,10 @@
 #include "PhysicsScene.h"
 #include <glm.hpp>
+#include "PhysicsObject.h"
 
-PhysicsScene::PhysicsScene()
+PhysicsScene::PhysicsScene() : gravity({ 0.0f, 0.0f }), timeStep(0.01f)
 {
-	SetGravity({0.0f, 0.0f});
-	SetTimeStep(0.01f);
+
 }
 
 PhysicsScene::~PhysicsScene()
@@ -18,20 +18,36 @@ PhysicsScene::~PhysicsScene()
 
 void PhysicsScene::AddActor(PhysicsObject* actor)
 {
-
+	actors.push_back(actor);
 }
 
 void PhysicsScene::RemoveActor(PhysicsObject* actor)
 {
-
+	auto a = std::find(actors.begin(), actors.end(), actor);
+	actors.erase(a);
+	delete* a;
 }
 
 void PhysicsScene::Update(float deltaTime)
 {
+	static float timePassed = 0.0f;
+	timePassed += deltaTime;
 
+	while (timePassed >= timeStep)
+	{
+		for (auto a : actors)
+		{
+			a->FixedUpdate(gravity, timeStep);
+		}
+
+		timePassed -= timeStep;
+	}
 }
 
 void PhysicsScene::Draw()
 {
-
+	for (auto a : actors)
+	{
+		a->Draw();
+	}
 }
