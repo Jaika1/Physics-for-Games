4e5d2efad1759e3fae26657d83b3cffbdac36bcd
Jaika1 <jackroennfeldtnospam@gmail.com>
2021-02-06 21:02:34 +1030

    Commenting work on PhysicsObject, including getting it ready for doxygen

--- CPPPhysicsSimulation/src/PhysicsObject.cpp
+++ CPPPhysicsSimulation/src/PhysicsObject.cpp
@@ -3,31 +3,88 @@
 
 collisionCheck_t PhysicsObject::collisionCheckFunctionArray[COLLISIONROWSIZE * COLLISIONROWSIZE] = 
 { 
-	&PhysicsObject::sphere2Sphere, &PhysicsObject::sphere2Sphere, &PhysicsObject::sphere2Sphere,
-	&PhysicsObject::sphere2Sphere, &PhysicsObject::sphere2Sphere, &PhysicsObject::sphere2Sphere,
-	&PhysicsObject::sphere2Sphere, &PhysicsObject::sphere2Sphere, &PhysicsObject::sphere2Sphere
+	&PhysicsObject::plane2Plane, &PhysicsObject::plane2Sphere, &PhysicsObject::plane2Box,
+	&PhysicsObject::sphere2Plane, &PhysicsObject::sphere2Sphere, &PhysicsObject::sphere2Box,
+	&PhysicsObject::box2Plane, &PhysicsObject::box2Sphere, &PhysicsObject::box2Box
 };
 
 bool PhysicsObject::checkCollision(PhysicsObject* actor1, PhysicsObject* actor2)
 {
+	// Grabs an ID from each actor that allows us to know what kind of shape it is.
 	ShapeType a1shape = actor1->m_shapeID;
 	ShapeType a2shape = actor2->m_shapeID;
 
+	// Call the appropriate function to handle checking for and managing a collision between the 2 different shapes.
 	return collisionCheckFunctionArray[(a1shape * COLLISIONROWSIZE) + a2shape](actor1, actor2);
+}
+
+#pragma region Collision Check Functions
+
+bool PhysicsObject::plane2Plane(PhysicsObject* actor1, PhysicsObject* actor2)
+{
+	// Planes are always static, so a collision between the 2 will never result in anything. This will also be the only collision function that doesnt check if the 2 actors are actually of the correct type.
+	return false;
+}
+
+bool PhysicsObject::plane2Sphere(PhysicsObject* actor1, PhysicsObject* actor2)
+{
+
+}
+
+bool PhysicsObject::plane2Box(PhysicsObject* actor1, PhysicsObject* actor2)
+{
+
+}
+
+
+bool PhysicsObject::sphere2Plane(PhysicsObject* actor1, PhysicsObject* actor2)
+{
+	// Use plane2Sphere with the parameters reversed.
+	return plane2Sphere(actor2, actor1);
 }
 
 bool PhysicsObject::sphere2Sphere(PhysicsObject* actor1, PhysicsObject* actor2)
 {
+	// Attempt to cast the actors into spheres
 	Sphere* sphere1 = dynamic_cast<Sphere*>(actor1);
 	Sphere* sphere2 = dynamic_cast<Sphere*>(actor2);
 
+	// If a cast was unsuccessful, it will be a nullptr. This if statement checks that for us.
 	if (sphere1 && sphere2)
 	{
+		// Get the distance between the center points of both spheres...
 		float sphereDist = glm::length(sphere2->getPosition() - sphere1->getPosition());
+		// Get the sum of both spheres radii...
 		float radiusSum = sphere1->getRadius() + sphere2->getRadius();
+		// And check to see if their distance is equal to or less than the sum of their radii!
 		return sphereDist <= radiusSum;
 	}
 
 	printf("sphere2Sphere collision function activated, but one or more are of the wrong type!\n");
 	return false;
 }
+
+bool PhysicsObject::sphere2Box(PhysicsObject* actor1, PhysicsObject* actor2)
+{
+
+}
+
+
+bool PhysicsObject::box2Plane(PhysicsObject* actor1, PhysicsObject* actor2)
+{
+	// Use plane2Box with the parameters reversed.
+	return plane2Box(actor2, actor1);
+}
+
+bool PhysicsObject::box2Sphere(PhysicsObject* actor1, PhysicsObject* actor2)
+{
+	// Use sphere2Box with the parameters reversed.
+	return sphere2Box(actor2, actor1);
+}
+
+bool PhysicsObject::box2Box(PhysicsObject* actor1, PhysicsObject* actor2)
+{
+
+}
+
+#pragma endregion
--- CPPPhysicsSimulation/src/PhysicsObject.h
+++ CPPPhysicsSimulation/src/PhysicsObject.h
@@ -1,10 +1,17 @@
 #pragma once
 #include <glm.hpp>
 
+// Foward decleration here is nessecary for the typedef below.
 class PhysicsObject;
 
+/// <summary>
+/// A type definition to point to a function that takes 2 PhysicsObject pointers, used to check and resolve collisions.
+/// </summary>
 typedef bool (*collisionCheck_t)(PhysicsObject* actor1, PhysicsObject* actor2);
 
+/// <summary>
+/// Contains entries for each shape type, to be used as IDs for discerning each one. Also has an entry for retreiving how many different shape types there are.
+/// </summary>
 enum ShapeType {
 	PLANE = 0,
 	SPHERE,
@@ -12,25 +19,115 @@ enum ShapeType {
 	COLLISIONROWSIZE
 };
 
+/// <summary>
+/// Represents the most basic form of an object that can be placed in the world. Designed to be inherited by other types, creating an instance of this on its own is heavily advised against. Also contains functions used for collision detection between different object types.
+/// </summary>
 class PhysicsObject
 {
 protected:
+	/// <summary>
+	/// Used to initialized this object and assign it an ID, which will be used for identifying it by other functions.
+	/// </summary>
+	/// <param name="shapeID">What type of shape this instance is.</param>
 	PhysicsObject(ShapeType shapeID) : m_shapeID(shapeID) {}
-	ShapeType m_shapeID;
+	/// <summary>
+	/// Represents the type of shape this is. Can only be set by the initializer.
+	/// </summary>
+	const ShapeType m_shapeID;
 
 public:
+	/// <summary>
+	/// Physics update, called when each fixed time-step has elapsed as defined in PhysicsScene. Must be overridden.
+	/// </summary>
+	/// <param name="gravity">The gravity vector.</param>
+	/// <param name="timeStep">The length of a time-step.</param>
 	virtual void fixedUpdate(glm::vec2 gravity, float timeStep) = 0;
+	/// <summary>
+	/// Draws the object in question. Must be overriden.
+	/// </summary>
 	virtual void draw() = 0;
+	/// <summary>
+	/// Currently unused, does absolutely nothing.
+	/// </summary>
 	virtual void resetPosition() {};
+	/// <summary>
+	/// Used to check and handle a potential collision between 2 different objects.
+	/// </summary>
+	/// <param name="actor1">A pointer to the first actor to test against actor2 for a collision.</param>
+	/// <param name="actor2">A pointer to the second actor to test against actor1 for a collision.</param>
+	/// <returns>True if a collision has occured, otherwise false.</returns>
 	static bool checkCollision(PhysicsObject* actor1, PhysicsObject* actor2);
 private:
+	/// <summary>
+	/// An array of function pointers that is used to determine the function required for collision checks between 2 PhysicsObject instances.
+	/// </summary>
 	static collisionCheck_t collisionCheckFunctionArray[];
 
-	//Plane & obj
+	#pragma region Collision Check Functions
+	/// <summary>
+	/// This function will always return false since planes are always static. This function exists because the pointer array we use requires it.
+	/// </summary>
+	/// <param name="actor1">A pointer to the first actor.</param>
+	/// <param name="actor2">A pointer to the second actor.</param>
+	/// <returns>Will always return false.</returns>
 	static bool plane2Plane(PhysicsObject* actor1, PhysicsObject* actor2);
+	/// <summary>
+	/// Attempts to cast actor 1 to a plane and actor 2 to a sphere, then checks for a collision between them.
+	/// </summary>
+	/// <param name="actor1">A pointer to the first actor to test against actor2 for a collision.</param>
+	/// <param name="actor2">A pointer to the second actor to test against actor1 for a collision.</param>
+	/// <returns>True if a collision has occured, otherwise false.</returns>
 	static bool plane2Sphere(PhysicsObject* actor1, PhysicsObject* actor2);
+	/// <summary>
+	/// Attempts to cast actor 1 to a plane and actor 2 to a box, then checks for a collision between them.
+	/// </summary>
+	/// <param name="actor1">A pointer to the first actor to test against actor2 for a collision.</param>
+	/// <param name="actor2">A pointer to the second actor to test against actor1 for a collision.</param>
+	/// <returns>True if a collision has occured, otherwise false.</returns>
+	static bool plane2Box(PhysicsObject* actor1, PhysicsObject* actor2);
 
-	//Sphere & obj
+	/// <summary>
+	/// Attempts to cast actor 1 to a sphere and actor 2 to a plane, then checks for a collision between them.
+	/// </summary>
+	/// <param name="actor1">A pointer to the first actor to test against actor2 for a collision.</param>
+	/// <param name="actor2">A pointer to the second actor to test against actor1 for a collision.</param>
+	/// <returns>True if a collision has occured, otherwise false.</returns>
 	static bool sphere2Plane(PhysicsObject* actor1, PhysicsObject* actor2);
+	/// <summary>
+	/// Attempts to cast both actors to spheres and checks for a collision between them.
+	/// </summary>
+	/// <param name="actor1">A pointer to the first actor to test against actor2 for a collision.</param>
+	/// <param name="actor2">A pointer to the second actor to test against actor1 for a collision.</param>
+	/// <returns>True if a collision has occured, otherwise false.</returns>
 	static bool sphere2Sphere(PhysicsObject* actor1, PhysicsObject* actor2);
+	/// <summary>
+	/// Attempts to cast actor 1 to a sphere and actor 2 to a box, then checks for a collision between them.
+	/// </summary>
+	/// <param name="actor1">A pointer to the first actor to test against actor2 for a collision.</param>
+	/// <param name="actor2">A pointer to the second actor to test against actor1 for a collision.</param>
+	/// <returns>True if a collision has occured, otherwise false.</returns>
+	static bool sphere2Box(PhysicsObject* actor1, PhysicsObject* actor2);
+
+	/// <summary>
+	/// Attempts to cast actor 1 to a box and actor 2 to a plane, then checks for a collision between them.
+	/// </summary>
+	/// <param name="actor1">A pointer to the first actor to test against actor2 for a collision.</param>
+	/// <param name="actor2">A pointer to the second actor to test against actor1 for a collision.</param>
+	/// <returns>True if a collision has occured, otherwise false.</returns>
+	static bool box2Plane(PhysicsObject* actor1, PhysicsObject* actor2);
+	/// <summary>
+	/// Attempts to cast actor 1 to a box and actor 2 to a sphere, then checks for a collision between them.
+	/// </summary>
+	/// <param name="actor1">A pointer to the first actor to test against actor2 for a collision.</param>
+	/// <param name="actor2">A pointer to the second actor to test against actor1 for a collision.</param>
+	/// <returns>True if a collision has occured, otherwise false.</returns>
+	static bool box2Sphere(PhysicsObject* actor1, PhysicsObject* actor2);
+	/// <summary>
+	/// Attempts to cast both actors to boxes and checks for a collision between them.
+	/// </summary>
+	/// <param name="actor1">A pointer to the first actor to test against actor2 for a collision.</param>
+	/// <param name="actor2">A pointer to the second actor to test against actor1 for a collision.</param>
+	/// <returns>True if a collision has occured, otherwise false.</returns>
+	static bool box2Box(PhysicsObject* actor1, PhysicsObject* actor2);
+	#pragma endregion
 };
