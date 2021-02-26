72dd30eb981fce0f7298996e05e072bce40292c3
Jack Roennfeldt <s200099@students.aie.edu.au>
2021-02-26 10:42:46 +1030

    Added unity project with the first tutorial completed

---
+++ UnityProject/Assets/Scripts/Cannonball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Cannonball : MonoBehaviour
+{
+    public float Speed = 20;
+
+    Rigidbody rBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        rBody.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (rBody.isKinematic)
+            {
+                rBody.isKinematic = false;
+                rBody.AddForce(transform.forward * Speed, ForceMode.VelocityChange);
+            }
+            else
+                rBody.AddForce(rBody.velocity.normalized * Speed, ForceMode.VelocityChange);
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/SleepVisualizer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SleepVisualizer : MonoBehaviour
+{
+    public Material AwakeMaterial;
+    public Material SleepMaterial;
+
+    Rigidbody rBody;
+    bool wasSleeping;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        wasSleeping = !rBody.IsSleeping();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (rBody.IsSleeping() && !wasSleeping && SleepMaterial != null)
+        {
+            wasSleeping = true;
+            GetComponent<MeshRenderer>().material = SleepMaterial;
+        }
+        
+        if (!rBody.IsSleeping() && wasSleeping && AwakeMaterial != null)
+        {
+            wasSleeping = false;
+            GetComponent<MeshRenderer>().material = AwakeMaterial;
+        }
+    }
+}
